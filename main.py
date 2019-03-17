4d03558e747a77dfd91180e4a6f12424c651efb2
Szymon Jakubiak <sz.a.jakubiak@gmail.com>
2019-03-17 19:07:53 +0100

    Rotary encoder for temperature scale change

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
+import board
+import digitalio
+import rotaryio
 from ir_camera import *
+from time import sleep
+
+# Initialize button used take a photo when connected to PC
+photo_btn = digitalio.DigitalInOut(board.D10)
+photo_btn.direction = digitalio.Direction.INPUT
+photo_btn.pull = digitalio.Pull.UP
+
+# Initialize rotary encoder used for temperature scale change
+encoder = rotaryio.IncrementalEncoder(board.A2, board.A3)
 
 set_scale()
 get_temp()
 sleep(0.25)
 
+enc_last_pos = encoder.position
+
 try:
     while True:
-        if not scale_btn.value: # when button pin value goes low
-            update_temp_max()
-            set_scale()
+        if not photo_btn.value: # when button pin value goes low
+            pixels.fill((0, 0, 0))
+            pixels.show()
             sleep(0.1)
+            send_temperatures()
+            sleep(0.1)
+            set_scale()
+        enc_pos = encoder.position
+        if enc_pos != enc_last_pos:
+            update_temp_max(enc_last_pos - enc_pos)
+            set_scale()
+            enc_last_pos = enc_pos
         set_matrix()
 except KeyboardInterrupt:
     pass
